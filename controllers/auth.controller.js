587387b86d7f1447dcec6b01a793b4d900e5da64
umut <umutcerk@gmail.com>
2025-06-09 23:20:37 +0300

    feat: add changeUserRole and getAllUsers functions with corresponding routes

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -171,11 +171,53 @@ const updateUserProfile = async (req, res) => {
     }
 };
 
+const changeUserRole = async (req, res) => {
+    try {
+        const { userId, newRole } = req.body;
+
+        if (!userId || !newRole) {
+            return res.status(400).json({ message: "User ID and new role are required" });
+        }
+
+        const user = await User.findByPk(userId);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        user.role = newRole;
+        await user.save();
+
+        return res.status(200).json({ message: "User role updated successfully", user: {
+            id: user.id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            role: user.role,
+        }});
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error", error: error.message });
+    }
+};
+
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.findAll({
+            attributes: ['id', 'firstName', 'lastName', 'email', 'role'],
+        });
+
+        return res.status(200).json(users);
+    } catch (error) {
+        return res.status(500).json({ message: "Internal server error", error: error.message });
+    }
+};
+
 export {
     register,
     login,
     logout,
     getCurrentUser,
     changePassword,
-    updateUserProfile
+    updateUserProfile,
+    changeUserRole,
+    getAllUsers
 };
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,14 +1,16 @@
 import express from 'express';
-import { register, login, logout, getCurrentUser, changePassword, updateUserProfile } from '../controllers/auth.controller.js';
-import { isAuth } from '../middlewares/authMiddleware.js';
+import { register, login, logout, getCurrentUser, changePassword, updateUserProfile, changeUserRole, getAllUsers } from '../controllers/auth.controller.js';
+import { isAuth, isAdmin } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
-router.post('/logout', logout);
+router.post('/logout', isAuth, logout);
 router.get('/me', isAuth, getCurrentUser);
 router.put('/change-password', isAuth, changePassword);
 router.put('/update-profile', isAuth, updateUserProfile);
+router.put('/change-role', isAuth, isAdmin, changeUserRole);
+router.get('/users', isAuth, isAdmin, getAllUsers);
 
 export default router;
